16979b7c443b1f675e8b5b28ddc9ee937a51966d
Adrian <52701496+futzu@users.noreply.github.com>
2022-08-31 00:52:31 +0000

    Update commands.go

--- commands.go
+++ commands.go
@@ -6,7 +6,7 @@ type SpliceCommand struct {
     	CommandType                 uint8
 	Identifier                  uint32  `json:",omitempty"`
 	Bites                       []byte  `json:",omitempty"`
-    SpliceEventID               string  `json:",omitempty"`
+    	SpliceEventID               string  `json:",omitempty"`
 	SpliceEventCancelIndicator  bool    `json:",omitempty"`
 	OutOfNetworkIndicator       bool    `json:",omitempty"`
 	ProgramSpliceFlag           bool    `json:",omitempty"`
@@ -25,7 +25,7 @@ type SpliceCommand struct {
 }
 
 // CommandDecoder returns a Command by cmdtype
-func (cmd *SpliceCommand) Decoder(cmdtype uint8,gob *Gob) {
+func (cmd *SpliceCommand) Decoder(cmdtype uint8,gob *gob.Gob) {
     cmd.CommandType = cmdtype
 	switch cmdtype {
 	case 0:
@@ -44,77 +44,19 @@ func (cmd *SpliceCommand) Decoder(cmdtype uint8,gob *Gob) {
 
 
 // Bandwidth Reservation
-func (cmd *SpliceCommand) BandwidthReservation(gob *Gob) {
+func (cmd *SpliceCommand) BandwidthReservation(gob *gob.Gob) {
 	cmd.Name = "Bandwidth Reservation"
 	gob.Forward(0)
 }
 // Private Command
-func (cmd *SpliceCommand) Private(gob *Gob) {
+func (cmd *SpliceCommand) Private(gob *gob.Gob) {
 	cmd.Name = "Private Command"
 	cmd.Identifier = gob.UInt32(32)
 	cmd.Bites = gob.AsBytes(24)
 }
 
 // Splice Null
-func (cmd *SpliceCommand) SpliceNull(gob *Gob) {
+func (cmd *SpliceCommand) SpliceNull(gob *gob.Gob) {
 	cmd.Name = "Splice Null"
 	gob.Forward(0)
-}
-
-// Splice Insert
-func (cmd *SpliceCommand) SpliceInsert(gob *Gob) {
-	cmd.Name = "Splice Insert"
-	cmd.SpliceEventID = gob.Hex(32)
-	cmd.SpliceEventCancelIndicator = gob.Bool()
-	gob.Forward(7)
-	if !cmd.SpliceEventCancelIndicator {
-		cmd.OutOfNetworkIndicator = gob.Bool()
-		cmd.ProgramSpliceFlag = gob.Bool()
-		cmd.DurationFlag = gob.Bool()
-		cmd.SpliceImmediateFlag = gob.Bool()
-		gob.Forward(4)
-	}
-	if cmd.ProgramSpliceFlag == true {
-		if !cmd.SpliceImmediateFlag {
-			cmd.spliceTime(gob)
-		}
-	} else {
-		cmd.ComponentCount = gob.UInt8(8)
-		var Components [256]uint8
-		cmd.Components = Components[0:cmd.ComponentCount]
-		for i := range cmd.Components {
-			cmd.Components[i] = gob.UInt8(8)
-		}
-		if !cmd.SpliceImmediateFlag {
-			cmd.spliceTime(gob)
-		}
-	}
-	if cmd.DurationFlag == true {
-		cmd.parseBreak(gob)
-	}
-	cmd.UniqueProgramID = gob.UInt16(16)
-	cmd.AvailNum = gob.UInt8(8)
-	cmd.AvailExpected = gob.UInt8(8)
-}
-
-func (cmd *SpliceCommand) parseBreak(gob *Gob) {
-	cmd.BreakAutoReturn = gob.Bool()
-	gob.Forward(6)
-	cmd.BreakDuration = gob.As90k(33)
-}
-
-func (cmd *SpliceCommand) spliceTime(gob *Gob) {
-	cmd.TimeSpecifiedFlag = gob.Bool()
-	if cmd.TimeSpecifiedFlag {
-		gob.Forward(6)
-		cmd.PTS = gob.As90k(33)
-	} else {
-		gob.Forward(7)
-	}
-}
-
-// Time Signal
-func (cmd *SpliceCommand) TimeSignal(gob *Gob) {
-	cmd.Name = "Time Signal"
-	cmd.spliceTime(gob)
 }
