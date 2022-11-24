ca52e5c389da8753ee42a971e86ffd397606bc11
Adrian <52701496+futzu@users.noreply.github.com>
2022-11-24 01:58:19 +0000

    packetData

--- cue.go
+++ cue.go
@@ -1,65 +1,49 @@
 package cuei
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
+	gobs "github.com/futzu/gob"
 )
 
-// chk generic catchall error checking
-func chk(e error) {
-	if e != nil {
-		fmt.Println(e)
-
-	}
+// Cue a SCTE35 cue.
+type Cue struct {
+	InfoSection
+	Command     SpliceCommand
+	Descriptors []SpliceDescriptor `json:",omitempty"`
+	PacketData      *packetData    `json:",omitempty"`
 }
 
-// DeB64 decodes base64 strings.
-func DeB64(b64 string) []byte {
-	deb64, err := base64.StdEncoding.DecodeString(b64)
-	chk(err)
-	return deb64
-}
-
-// IsIn is a test for slice membership
-func IsIn(slice []uint16, val uint16) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
+// Decode extracts bits for the Cue values.
+func (cue *Cue) Decode(bites []byte) bool {
+	var gob gobs.Gob
+	gob.Load(bites)
+	if cue.InfoSection.Decode(&gob) {
+		cue.Command.Decode(cue.InfoSection.SpliceCommandType, &gob)
+		cue.InfoSection.DescriptorLoopLength = gob.UInt16(16)
+		cue.dscptrLoop(&gob)
+		return true
 	}
 	return false
 }
 
-func mk90k(raw uint64) float64 {
-	nk := float64(raw) / 90000.0
-	return float64(uint64(nk*1000000)) / 1000000
-}
-
-// MkJson structs to JSON
-func MkJson(i interface{}) string {
-	jason, err := json.MarshalIndent(&i, "", "    ")
-	chk(err)
-	return string(jason)
-}
-
-func parseLen(byte1, byte2 byte) uint16 {
-	return uint16(byte1&0xf)<<8 | uint16(byte2)
-}
-
-func parsePid(byte1, byte2 byte) uint16 {
-	return uint16(byte1&0x1f)<<8 | uint16(byte2)
-}
-
-func parsePrgm(byte1, byte2 byte) uint16 {
-	return uint16(byte1)<<8 | uint16(byte2)
-}
-
-func splitByIdx(payload, sep []byte) []byte {
-	idx := bytes.Index(payload, sep)
-	if idx == -1 {
-		return []byte("")
+// DscptrLoop loops over any splice descriptors
+func (cue *Cue) dscptrLoop(gob *gobs.Gob) {
+	var i uint16
+	i = 0
+	l := cue.InfoSection.DescriptorLoopLength
+	for i < l {
+		tag := gob.UInt8(8)
+		i++
+		length := gob.UInt16(8)
+		i++
+		i += length
+		var sdr SpliceDescriptor
+		sdr.Decode(gob, tag, uint8(length))
+		cue.Descriptors = append(cue.Descriptors, sdr)
 	}
-	return payload[idx:]
+}
+
+// Show display SCTE-35 data as JSON.
+func (cue *Cue) Show() {
+	fmt.Println(MkJson(&cue))
 }
