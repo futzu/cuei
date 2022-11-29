0cf2f1dc90a3b09e1065d0b38d9dd7fdcc1af71f
Adrian <52701496+futzu@users.noreply.github.com>
2022-11-29 17:24:20 +0000

    updated docs

--- scte35parser.go
+++ scte35parser.go
@@ -1,35 +1,39 @@
 package cuei
 
-// Scte35Parser is for incorporating with another MPEGTS parser.
+/*
+	*
+	   Scte35Parser is for incorporating with another MPEGTS parser.
+	       Usage:
+
+	       import(
+	           "github.com/futzu/cuei"
+	           )
+	       scte35parser := cuei.Scte35Parser{}
+
+	           // Each time your parser/demuxer finds a SCTE-35 packet (stream type 0x86)
+
+	           // do something like
+
+	           cue := scte35parser.Parse(aScte35Packet)
+	           if cue != nil {
+	                   // do something with the cue
+	           }
+
+*
+*/
 type Scte35Parser struct {
 	Stream
 }
 
 /*
-	*
+*
+
 	Parse accepts a pkt as input.
 
-	If the packet is a partial Cue,
-	it will be stored and aggregated
-	with the next packet until complete.
+	    If the packet is a partial Cue, it will be stored and aggregated
+	    with the next packet until complete.
 
-	Completed packet(s)
-	with be decoded into a Cue and returned.
-
-	Usage:
-
-	 import(
-	   "github.com/futzu/cuei"
-	 )
-	 scte35parser := Scte35Parser{}
-	 // parse mpegts for scte35 packets with your MPEGTS parser.
-	 ...
-
-
-	 cue := scte35parser.Parse(aScte35Packet)
-	 if cue != nil {
-	     // do something with the cue
-	 }
+	    completed packet(s) with be decoded into a Cue and returned.
 
 *
 */
