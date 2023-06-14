02d527ef9e18ab694f21745094e3c1a40bd57d34
Adrian <52701496+futzu@users.noreply.github.com>
2023-06-14 05:27:14 +0000

    Update nbin.go docs

--- nbin.go
+++ nbin.go
@@ -5,11 +5,12 @@ import (
 	"math/big"
 )
 
+// Nbin packs SCTE-35 data as bits for encoding and stores them in a big.Int called Bites
 type Nbin struct {
 	Bites big.Int
 }
 
-// Add bytes to NBin.Bites for encoding
+// Append a string  as bits to NBin.Bites for encoding
 func (nb *Nbin) AddBytes(str string, nbits uint) {
 	t := new(big.Int)
 	t.SetBytes([]byte(str))
@@ -17,7 +18,7 @@ func (nb *Nbin) AddBytes(str string, nbits uint) {
 	nb.Bites = *nb.Bites.Add(o, t)
 }
 
-// Add64 left shift NBin.Bites by nbits and add uint64 val
+// Add64  append  a uint64 as bits to NBin.Bites
 func (nb *Nbin) Add64(val uint64, nbits uint) {
 	t := new(big.Int)
 	t.SetUint64(val)
@@ -25,25 +26,25 @@ func (nb *Nbin) Add64(val uint64, nbits uint) {
 	nb.Bites = *nb.Bites.Add(o, t)
 }
 
-// Add32 left shift NBin.Bites by nbits and add uint32 val
+// Add32  append a  unit32 as bits to NBin.Bites
 func (nb *Nbin) Add32(val uint32, nbits uint) {
 	u := uint64(val)
 	nb.Add64(u, nbits)
 }
 
-// Add16 left shift NBin.Bites by nbits and add uint16 val
+// Add16  append a  unit16 as bits to NBin.Bites
 func (nb *Nbin) Add16(val uint16, nbits uint) {
 	u := uint64(val)
 	nb.Add64(u, nbits)
 }
 
-// Add8 left shift NBin.Bites by nbits and add uint8 val
+//  Add8  append a  unit8 as bits to NBin.Bites
 func (nb *Nbin) Add8(val uint8, nbits uint) {
 	u := uint64(val)
 	nb.Add64(u, nbits)
 }
 
-// AddFlag left shift NBin.Bites by 1 and add bool val
+// AddFlag append a bool as a bit to NBin.Bytes
 func (nb *Nbin) AddFlag(val bool) {
 	if val == true {
 		nb.Add64(1, 1)
@@ -52,13 +53,13 @@ func (nb *Nbin) AddFlag(val bool) {
 	}
 }
 
-// Add90k left shift NBin.Bites by nbits and add val as ticks
+// Add90k append a 90k clock value, as ticks, in bits, to NBin.Bytes
 func (nb *Nbin) Add90k(val float64, nbits uint) {
 	u := uint64(val * float64(90000.0))
 	nb.Add64(u, nbits)
 }
 
-// AddHex64 left shift NBin.Bites by nbits and add hex string as uint64
+// AddHex64 append a hex string, as uint64, in bits, to NBin.Bites 
 func (nb *Nbin) AddHex64(val string, nbits uint) {
 	u := new(big.Int)
 	_, err := fmt.Sscan(val, u)
