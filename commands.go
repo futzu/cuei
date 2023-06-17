1cb08d28013c5a7750960453311c1d3254b5bc14
Adrian <52701496+futzu@users.noreply.github.com>
2023-06-17 06:12:19 +0000

    Update commands.go

--- commands.go
+++ commands.go
@@ -1,130 +1,270 @@
 package cuei
 
 import (
-	"fmt"
 	gobs "github.com/futzu/gob"
 )
 
 /*
 *
-Cue is a SCTE35 cue.
+SpliceCommand
 
-	A Cue contains:
-	    1 InfoSection
-	    1 SpliceCommand
-	    0 or more Splice Descriptors
-	    1 packetData (if parsed from MPEGTS)
+	These Splice Command types are consolidated into SpliceCommand.
+
+	     0x0: Splice Null,
+	     0x5: Splice Insert,
+	     0x6: Time Signal,
+	     0x7: Bandwidth Reservation,
+	     0xff: Private,
 
 *
 */
-type Cue struct {
-	Bites       []byte
-	InfoSection *InfoSection
-	Command     *SpliceCommand
-	Descriptors []SpliceDescriptor `json:",omitempty"`
-	PacketData  *packetData        `json:",omitempty"`
+type SpliceCommand struct {
+	Name                       string
+	CommandType                uint8
+	Identifier                 uint32  `json:",omitempty"`
+	Bites                      []byte  `json:",omitempty"`
+	SpliceEventID              string  `json:",omitempty"`
+	SpliceEventCancelIndicator bool    `json:",omitempty"`
+	OutOfNetworkIndicator      bool    `json:",omitempty"`
+	ProgramSpliceFlag          bool    `json:",omitempty"`
+	DurationFlag               bool    `json:",omitempty"`
+	BreakAutoReturn            bool    `json:",omitempty"`
+	BreakDuration              float64 `json:",omitempty"`
+	SpliceImmediateFlag        bool    `json:",omitempty"`
+	ComponentCount             uint8   `json:",omitempty"`
+	Components                 []uint8 `json:",omitempty"`
+	UniqueProgramID            uint16  `json:",omitempty"`
+	AvailNum                   uint8   `json:",omitempty"`
+	AvailExpected              uint8   `json:",omitempty"`
+	TimeSpecifiedFlag          bool    `json:",omitempty"`
+	PTS                        float64 `json:",omitempty"`
 }
 
-// Decode extracts bits for the Cue values.
-func (cue *Cue) Decode(bites []byte) bool {
-	var gob gobs.Gob
-	gob.Load(bites)
-	cue.InfoSection = &InfoSection{}
-	if cue.InfoSection.Decode(&gob) {
-		cue.Command = &SpliceCommand{}
-		cue.Command.Decode(cue.InfoSection.SpliceCommandType, &gob)
-		cue.InfoSection.DescriptorLoopLength = gob.UInt16(16)
-		cue.dscptrLoop(&gob)
-		return true
+// Decode returns a Command by type
+func (cmd *SpliceCommand) Decode(cmdtype uint8, gob *gobs.Gob) {
+	cmd.CommandType = cmdtype
+	switch cmdtype {
+	case 0x0:
+		cmd.decodeSpliceNull(gob)
+	case 0x5:
+		cmd.decodeSpliceInsert(gob)
+	case 0x6:
+		cmd.decodeTimeSignal(gob)
+	case 0x7:
+		cmd.decodeBandwidthReservation(gob)
+	case 0xff:
+		cmd.decodePrivate(gob)
 	}
-	return false
+
 }
 
-// DscptrLoop loops over any splice descriptors
-func (cue *Cue) dscptrLoop(gob *gobs.Gob) {
-	var i uint16
-	i = 0
-	l := cue.InfoSection.DescriptorLoopLength
-	for i < l {
-		tag := gob.UInt8(8)
-		i++
-		length := gob.UInt16(8)
-		i++
-		i += length
-		var sdr SpliceDescriptor
-		sdr.Decode(gob, tag, uint8(length))
-		cue.Descriptors = append(cue.Descriptors, sdr)
+// Encode returns a SpliceCommand values as bytes
+func (cmd *SpliceCommand) Encode() []byte {
+	var fu []byte
+
+	switch cmd.CommandType {
+	case 0x5:
+		fu = cmd.encodeSpliceInsert()
+	}
+	return fu
+}
+
+// bandwidth Reservation
+func (cmd *SpliceCommand) decodeBandwidthReservation(gob *gobs.Gob) {
+	cmd.Name = "Bandwidth Reservation"
+	gob.Forward(0)
+}
+
+// private Command
+func (cmd *SpliceCommand) decodePrivate(gob *gobs.Gob) {
+	cmd.Name = "Private Command"
+	cmd.Identifier = gob.UInt32(32)
+	cmd.Bites = gob.Bytes(24)
+}
+
+// splice Null
+func (cmd *SpliceCommand) decodeSpliceNull(gob *gobs.Gob) {
+	cmd.Name = "Splice Null"
+	gob.Forward(0)
+}
+
+// splice Insert
+func (cmd *SpliceCommand) decodeSpliceInsert(gob *gobs.Gob) {
+	cmd.Name = "Splice Insert"
+	cmd.SpliceEventID = gob.Hex(32)
+	cmd.SpliceEventCancelIndicator = gob.Flag()
+	gob.Forward(7)
+	if !cmd.SpliceEventCancelIndicator {
+		cmd.OutOfNetworkIndicator = gob.Flag()
+		cmd.ProgramSpliceFlag = gob.Flag()
+		cmd.DurationFlag = gob.Flag()
+		cmd.SpliceImmediateFlag = gob.Flag()
+		gob.Forward(4)
+	}
+	if cmd.ProgramSpliceFlag == true {
+		if !cmd.SpliceImmediateFlag {
+			cmd.spliceTime(gob)
+		}
+	} else {
+		cmd.ComponentCount = gob.UInt8(8)
+		var Components [256]uint8
+		cmd.Components = Components[0:cmd.ComponentCount]
+		for i := range cmd.Components {
+			cmd.Components[i] = gob.UInt8(8)
+		}
+		if !cmd.SpliceImmediateFlag {
+			cmd.spliceTime(gob)
+		}
+	}
+	if cmd.DurationFlag == true {
+		cmd.parseBreak(gob)
+	}
+	cmd.UniqueProgramID = gob.UInt16(16)
+	cmd.AvailNum = gob.UInt8(8)
+	cmd.AvailExpected = gob.UInt8(8)
+}
+
+func (cmd *SpliceCommand) parseBreak(gob *gobs.Gob) {
+	cmd.BreakAutoReturn = gob.Flag()
+	gob.Forward(6)
+	cmd.BreakDuration = gob.As90k(33)
+}
+
+func (cmd *SpliceCommand) spliceTime(gob *gobs.Gob) {
+	cmd.TimeSpecifiedFlag = gob.Flag()
+	if cmd.TimeSpecifiedFlag {
+		gob.Forward(6)
+		cmd.PTS = gob.As90k(33)
+	} else {
+		gob.Forward(7)
 	}
 }
 
-// Show display SCTE-35 data as JSON.
-func (cue *Cue) Show() {
-	fmt.Println(MkJson(&cue))
+// MkSpliceInsert easy Splice Insert Splice Command creation
+/**
+
+
+    The args set the SpliceInsert vars.
+
+    SpliceEventID = eventid
+
+    if pts is 0 (default):
+        SpliceImmediateFlag      True
+        timeSpecifiedFlag      False
+
+    if pts > 0:
+        spliceImmediateFlag      False
+        timeSpecifiedFlag       True
+        PTS                  pts
+
+    If duration is 0(default)
+        DurationFlag              False
+
+    if duration IS set:
+        OutOfNetworkIndicator   True
+        durationFlag           True
+        BreakAutoReturn         True
+        BreakDuration             duration
+        PTS                  pts
+
+    if out :
+        OutOfNetworkIndicator  True
+
+    if out is False (default):
+        OutOfNetworkIndicator   False
+
+**/
+func (cmd *SpliceCommand) MkSpliceInsert(eventid string, pts float64, duration float64, out bool) {
+	cmd.Name = "Splice Insert"
+	cmd.CommandType = 5
+	cmd.SpliceEventID = eventid
+	cmd.SpliceEventCancelIndicator = false
+	cmd.ProgramSpliceFlag = false
+	cmd.SpliceImmediateFlag = true
+	cmd.DurationFlag = false
+	cmd.AvailNum = 0
+	cmd.AvailExpected = 0
+	cmd.TimeSpecifiedFlag = false
+	if pts > 0 {
+		cmd.ProgramSpliceFlag = true
+		cmd.SpliceImmediateFlag = false
+		cmd.TimeSpecifiedFlag = true
+		cmd.PTS = pts
+	} else {
+		cmd.ComponentCount = 0
+	}
+	if duration > 0 {
+		cmd.DurationFlag = true
+		cmd.BreakDuration = duration
+		cmd.BreakAutoReturn = true
+		cmd.OutOfNetworkIndicator = true
+	}
+
 }
 
-func (cue *Cue) Encode() string {
-	cue.Command.Encode()
-	cmdl := len(cue.Command.Bites)
-	cue.InfoSection.SpliceCommandLength = uint16(cmdl)
-	cue.InfoSection.SpliceCommandType = cue.Command.CommandType
-	// 11 bytes for info section + command + 2 descriptor loop length
-	// + descriptor loop + 4 for crc
-	cue.InfoSection.SectionLength = uint16(11 + cmdl + 2 + 4)
-	cue.InfoSection.Encode()
+// encode Splice Insert Splice Command
+func (cmd *SpliceCommand) encodeSpliceInsert() []byte {
 	nb := &Nbin{}
-	isbits := uint(len(cue.InfoSection.Bites) << 3)
-	nb.AddBytes(string(cue.InfoSection.Bites), isbits)
-	ccbits := uint(len(cue.Command.Bites) << 3)
-	nb.AddBytes(string(cue.Command.Bites), ccbits)
-	dll := uint8(0)
-	nb.Add8(dll, 2)
-	/**     cue.Bites += int.to_bytes(
-	            self.info_section.descriptor_loop_length, 2, byteorder="big"
-	        )
-	        self.bites += dscptr_bites
-
-	**/
-	nb.Add32(CRC32(cue.Bites), 32)
-	cue.Bites = nb.Bites.Bytes()
-	return EncB64(cue.Bites)
-}
-
-/*
-  - Six2Five converts a Cue with a Time Sgnal Command
-    and a Segmentation Descriptor with a
-    type id of 0x34,0x35,0x36,0x37,0x38. or 0x39
-    into a Cue with a Splice Insert Command.
-
-*
-*/
-func (cue *Cue) Six2Five() {
-	upidStarts := []uint16{0x34, 0x36, 0x38}
-	upidStops := []uint16{0x35, 0x37, 0x39}
-	eventid := "0x0"
-	pts := 0.0
-	duration := float64(0.0)
-	out := false
-	if cue.Command.CommandType == 6 {
-		if cue.Command.PTS > 0.0 {
-			pts = cue.Command.PTS
+	nb.AddHex64(cmd.SpliceEventID, 32)
+	nb.AddFlag(cmd.SpliceEventCancelIndicator)
+	nb.Reserve(7)
+	if !cmd.SpliceEventCancelIndicator {
+		nb.AddFlag(cmd.OutOfNetworkIndicator)
+		nb.AddFlag(cmd.ProgramSpliceFlag)
+		nb.AddFlag(cmd.DurationFlag)
+		nb.AddFlag(cmd.SpliceImmediateFlag)
+		nb.Reserve(4)
+	}
+	if cmd.ProgramSpliceFlag == true {
+		if !cmd.SpliceImmediateFlag {
+			cmd.encodeSpliceTime(nb)
 		}
-		for _, dscptr := range cue.Descriptors {
-			if dscptr.Tag == 2 {
-				//value, _ := strconv.ParseInt(hex, 16, 64)
-				eventid = fmt.Sprintf("%v", Hex2Int(dscptr.SegmentationEventID)&uint64(2^31))
-				if IsIn(upidStarts, uint16(dscptr.SegmentationTypeID)) {
-					if dscptr.SegmentationDurationFlag {
-						duration = dscptr.SegmentationDuration
-						out = true
-					}
-				}
-				if IsIn(upidStops, uint16(dscptr.SegmentationTypeID)) {
-					out = false
-				}
-			}
-			cue.Command.MkSpliceInsert(eventid, pts, duration, out)
-			cue.show()
-			fmt.Println("Six 2 Five")
+	} else {
+		nb.Add8(cmd.ComponentCount, 8)
+		for i := range cmd.Components {
+			nb.Add8(cmd.Components[i], 8)
+		}
+		if !cmd.SpliceImmediateFlag {
+			cmd.encodeSpliceTime(nb)
 		}
 	}
+	if cmd.DurationFlag == true {
+		cmd.encodeBreak(nb)
+	}
+	nb.Add16(cmd.UniqueProgramID, 16)
+	nb.Add8(cmd.AvailNum, 8)
+	nb.Add8(cmd.AvailExpected, 8)
+
+	return nb.Bites.Bytes()
+}
+
+func (cmd *SpliceCommand) encodeBreak(nb *Nbin) {
+	nb.AddFlag(cmd.BreakAutoReturn)
+	nb.Reserve(6)
+	nb.Add90k(cmd.BreakDuration, 33)
+}
+
+// encode PTS splice times
+func (cmd *SpliceCommand) encodeSpliceTime(nb *Nbin) {
+	nb.AddFlag(cmd.TimeSpecifiedFlag)
+	if cmd.TimeSpecifiedFlag {
+		nb.Reserve(6)
+		nb.Add90k(cmd.PTS, 33)
+	} else {
+		nb.Reserve(7)
+	}
+}
+
+// decode Time Signal Splice Commands
+func (cmd *SpliceCommand) decodeTimeSignal(gob *gobs.Gob) {
+	cmd.Name = "Time Signal"
+	cmd.spliceTime(gob)
+}
+
+// encode Time Signal Splice Commands
+func (cmd *SpliceCommand) encodeTimeSignal() []byte {
+	nb := &Nbin{}
+	cmd.encodeSpliceTime(nb)
+	cmd.Bites = nb.Bites.Bytes()
+	return nb.Bites.Bytes()
 }
