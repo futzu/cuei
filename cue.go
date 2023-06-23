7234ea8cc5161a7b48143a0358cf5bd931b1ad02
Adrian <52701496+futzu@users.noreply.github.com>
2023-06-23 02:53:35 +0000

    Encoding now works for splice insert and time signal

--- cue.go
+++ cue.go
@@ -12,7 +12,9 @@ Cue is a SCTE35 cue.
 	A Cue contains:
 	    1 InfoSection
 	    1 SpliceCommand
+	    1 Dll  (Descriptor loop length)
 	    0 or more Splice Descriptors
+	    1 Crc32
 	    1 packetData (if parsed from MPEGTS)
 
 *
@@ -21,8 +23,10 @@ type Cue struct {
 	Bites       []byte
 	InfoSection *InfoSection
 	Command     *SpliceCommand
+	Dll         uint16
 	Descriptors []SpliceDescriptor `json:",omitempty"`
 	PacketData  *packetData        `json:",omitempty"`
+	Crc32       uint32
 }
 
 // Decode extracts bits for the Cue values.
@@ -33,18 +37,19 @@ func (cue *Cue) Decode(bites []byte) bool {
 	if cue.InfoSection.Decode(&gob) {
 		cue.Command = &SpliceCommand{}
 		cue.Command.Decode(cue.InfoSection.SpliceCommandType, &gob)
-		cue.InfoSection.DescriptorLoopLength = gob.UInt16(16)
-		cue.dscptrLoop(&gob)
+		cue.Dll = gob.UInt16(16)
+		cue.dscptrLoop(cue.Dll, &gob)
+		cue.Crc32 = gob.UInt32(32)
 		return true
 	}
 	return false
 }
 
 // DscptrLoop loops over any splice descriptors
-func (cue *Cue) dscptrLoop(gob *gobs.Gob) {
+func (cue *Cue) dscptrLoop(dll uint16, gob *gobs.Gob) {
 	var i uint16
 	i = 0
-	l := cue.InfoSection.DescriptorLoopLength
+	l := dll
 	for i < l {
 		tag := gob.UInt8(8)
 		i++
@@ -62,14 +67,10 @@ func (cue *Cue) Show() {
 	fmt.Println(MkJson(&cue))
 }
 
+// Encode Cue currently works for Splice Inserts and Time Signals
 func (cue *Cue) Encode() []byte {
-	//cmdl := len(cue.Command.Bites)
-	//cue.Command.MkSpliceInsert("77",11.012344,30.1,true)
 	cmdb := cue.Command.Encode()
 	cmdl := len(cmdb)
-	//fmt.Println(cmdb)
-
-	cue.InfoSection.Defaults()
 	cue.InfoSection.SpliceCommandLength = uint16(cmdl)
 	cue.InfoSection.SpliceCommandType = cue.Command.CommandType
 	// 11 bytes for info section + command + 2 descriptor loop length
@@ -81,63 +82,9 @@ func (cue *Cue) Encode() []byte {
 	nb.AddBytes(cue.InfoSection.Bites, isbits)
 	ccbits := uint(len(cmdb) << 3)
 	nb.AddBytes(cmdb, ccbits)
-	dll := uint16(0)
-	nb.Add16(dll, 16)
-	/**     cue.Bites += int.to_bytes(
-	            self.info_section.descriptor_loop_length, 2, byteorder="big"
-	        )
-	        self.bites += dscptr_bites
-
-	**/
-	crc32 := CRC32(nb.Bites.Bytes())
-	nb.Add32(crc32, 32)
+	nb.Add16(0, 16)
+	cue.Crc32 = CRC32(nb.Bites.Bytes())
+	nb.Add32(cue.Crc32, 32)
 	cue.Bites = nb.Bites.Bytes()
 	return nb.Bites.Bytes()
-}
-
-/*
-  - Six2Five converts a Cue with a Time Sgnal Command
-    and a Segmentation Descriptor with a
-    type id of 0x34,0x35,0x36,0x37,0x38. or 0x39
-    into a Cue with a Splice Insert Command.
-
-*
-*/
-func (cue *Cue) Six2Five() {
-	upidStarts := []uint16{0x34, 0x36, 0x38}
-	upidStops := []uint16{0x35, 0x37, 0x39}
-	eventid := "0x0"
-	pts := 0.0
-	duration := float64(0.0)
-	out := false
-	if cue.Command.CommandType == 6 {
-		cue.Command.CommandType = 5
-
-		cue.Command.Name = "Six2Five'd Splice Insert"
-		if cue.Command.PTS > 0.0 {
-			pts = cue.Command.PTS
-		}
-		for _, dscptr := range cue.Descriptors {
-			if dscptr.Tag == 2 {
-				//value, _ := strconv.ParseInt(hex, 16, 64)
-				eventid = fmt.Sprintf("%v", Hex2Int(dscptr.SegmentationEventID)&uint64(2^31))
-				if IsIn(upidStarts, uint16(dscptr.SegmentationTypeID)) {
-					if dscptr.SegmentationDurationFlag {
-						duration = dscptr.SegmentationDuration
-						out = true
-					}
-				}
-				if IsIn(upidStops, uint16(dscptr.SegmentationTypeID)) {
-					out = false
-				}
-			}
-			cue.Command.MkSpliceInsert(eventid, pts, duration, out)
-			cue.Command.Name = "Six2Five'd Splice Insert"
-
-			cue.Encode()
-			//cue.Decode(cue.Encode())
-			cue.Show()
-			fmt.Println("Six 2 Five")
-		}
-	}
 }
