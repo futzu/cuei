33311b6f99f73831c6b1cd607c1f5b5dbe176f67
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-08 21:20:31 +0000

    backward compatibility

---
+++ backward.go
@@ -0,0 +1,12 @@
+package cuei
+
+
+// SpliceCommand for backward compatibility
+type SpliceCommand struct {
+		Command
+}
+
+// SpliceDescriptor for backward compatibility
+type SpliceDescriptor struct {
+		Descriptor
+}
