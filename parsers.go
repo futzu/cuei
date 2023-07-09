c397aa90d1f94230439727ab3675324fd14eeeb8
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-09 19:26:02 +0000

    Add files via upload

---
+++ parsers.go
@@ -0,0 +1,41 @@
+package cuei
+
+// Cue Parser is wrapper for Cue
+type CueParser struct {
+	Cue
+}
+
+// Parse decodes bites into SCTE-35 values
+func (cuep *CueParser) Parse(bites []byte) {
+	cuep.Decode(bites)
+}
+
+// Show prints the Cue values as JSON
+func (cuep *CueParser) Show() {
+	cuep.Show()
+}
+
+// initialize and return a *CueParser
+func NewCueParser() *CueParser {
+	cuep := &CueParser{}
+	return cuep
+}
+
+// StreamParser parses a []byte of mpegts for SCTE-35
+type StreamParser struct {
+	Stream
+}
+
+// Parse parses  mpegts bytes and returns any SCTE-35 Cues found
+func (streamp *StreamParser) Parse(bites []byte) []*Cue {
+	cues := streamp.DecodeBytes(bites)
+	return cues
+}
+
+// initialize and return a *StreamParser
+func NewStreamParser() *StreamParser {
+	sp := &StreamParser{}
+	sp.Pids = &Pids{}
+	sp.mkMaps()
+	return sp
+}
