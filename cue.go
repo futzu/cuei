bc4989e83d57701e26e624d5d50f588153f08b6f
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-10 19:34:26 +0000

    Switched to bitter

--- cue.go
+++ cue.go
@@ -2,7 +2,7 @@ package cuei
 
 import (
 	"fmt"
-	gobs "github.com/futzu/gob"
+	bitter "github.com/futzu/bitter"
 )
 
 /*
@@ -10,12 +10,13 @@ import (
 Cue is a SCTE35 cue.
 
 A Cue contains:
- 	1 InfoSection
-    	1 Command
-    	1 Dll  Descriptor loop length
-    	0 or more Splice Descriptors
-    	1 Crc32
-    	1 packetData (if parsed from MPEGTS)
+
+		1 InfoSection
+	   	1 Command
+	   	1 Dll  Descriptor loop length
+	   	0 or more Splice Descriptors
+	   	1 Crc32
+	   	1 packetData (if parsed from MPEGTS)
 
 *
 */
@@ -30,33 +31,33 @@ type Cue struct {
 
 // Decode extracts bits for the Cue values.
 func (cue *Cue) Decode(bites []byte) bool {
-	var gob gobs.Gob
-	gob.Load(bites)
+	var bd bitter.Decoder
+	bd.Load(bites)
 	cue.InfoSection = &InfoSection{}
-	if cue.InfoSection.Decode(&gob) {
+	if cue.InfoSection.Decode(&bd) {
 		cue.Command = &Command{}
-		cue.Command.Decode(cue.InfoSection.CommandType, &gob)
-		cue.Dll = gob.UInt16(16)
-		cue.dscptrLoop(cue.Dll, &gob)
-		cue.Crc32 = gob.UInt32(32)
+		cue.Command.Decode(cue.InfoSection.CommandType, &bd)
+		cue.Dll = bd.UInt16(16)
+		cue.dscptrLoop(cue.Dll, &bd)
+		cue.Crc32 = bd.UInt32(32)
 		return true
 	}
 	return false
 }
 
 // DscptrLoop loops over any splice descriptors
-func (cue *Cue) dscptrLoop(dll uint16, gob *gobs.Gob) {
+func (cue *Cue) dscptrLoop(dll uint16, bd *bitter.Decoder) {
 	var i uint16
 	i = 0
 	l := dll
 	for i < l {
-		tag := gob.UInt8(8)
+		tag := bd.UInt8(8)
 		i++
-		length := gob.UInt16(8)
+		length := bd.UInt16(8)
 		i++
 		i += length
 		var sdr Descriptor
-		sdr.Decode(gob, tag, uint8(length))
+		sdr.Decode(bd, tag, uint8(length))
 		cue.Descriptors = append(cue.Descriptors, sdr)
 	}
 }
@@ -76,16 +77,15 @@ func (cue *Cue) Encode() []byte {
 	// + descriptor loop + 4 for crc
 	cue.InfoSection.SectionLength = uint16(11 + cmdl + 2 + 4)
 	isecb := cue.InfoSection.Encode()
-	nb := &Nbin{}
+	be := &bitter.Encoder{}
 	isecbits := uint(len(isecb) << 3)
-	nb.AddBytes(isecb, isecbits)
+	be.AddBytes(isecb, isecbits)
 	cmdbits := uint(cmdl << 3)
-	nb.AddBytes(cmdb, cmdbits)
-	nb.Add16(0, 16) // descriptor loop currently disabled for encoding
-	cue.Crc32 = CRC32(nb.Bites.Bytes())
-	nb.Add32(cue.Crc32, 32)
-	//cue.Bites = nb.Bites.Bytes()
-	return nb.Bites.Bytes()
+	be.AddBytes(cmdb, cmdbits)
+	be.Add16(0, 16) // descriptor loop currently disabled for encoding
+	cue.Crc32 = CRC32(be.Bites.Bytes())
+	be.Add32(cue.Crc32, 32)
+	return be.Bites.Bytes()
 }
 
 // used by Six2Five to convert a time signal to a splice insert
@@ -139,8 +139,8 @@ func (cue *Cue) mkSpliceInsert() {
 *
 */
 func (cue *Cue) Six2Five() string {
-	segStarts := []uint16{0x22,0x30,0x32, 0x34,0x36, 0x38,0x3a,0x3c,0x3e,0x44,0x46}
-	segStops := []uint16{0x23,0x31,0x33,0x35, 0x37, 0x39,0x3b,0x3d,0x3f,0x45,0x47}
+	segStarts := []uint16{0x22, 0x30, 0x32, 0x34, 0x36, 0x38, 0x3a, 0x3c, 0x3e, 0x44, 0x46}
+	segStops := []uint16{0x23, 0x31, 0x33, 0x35, 0x37, 0x39, 0x3b, 0x3d, 0x3f, 0x45, 0x47}
 	if cue.InfoSection.CommandType == 6 {
 		for _, dscptr := range cue.Descriptors {
 			if dscptr.Tag == 2 {
