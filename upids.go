dc4cc8a2f844ef654c547b76b917f2d0d70b0acb
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-12 22:17:24 +0000

    Update upids.go

--- upids.go
+++ upids.go
@@ -1,90 +1,158 @@
 package cuei
 
 import (
+	"fmt"
 	bitter "github.com/futzu/bitter"
 )
 
-// InfoSection is the splice info section of the SCTE 35 cue.
-type InfoSection struct {
-	Name                   string
-	TableID                string
-	SectionSyntaxIndicator bool
-	Private                bool
-	Reserved               string
-	SectionLength          uint16
-	ProtocolVersion        uint8
-	EncryptedPacket        bool
-	EncryptionAlgorithm    uint8
-	PtsAdjustment          float64
-	CwIndex                string
-	Tier                   string
-	CommandLength          uint16
-	CommandType            uint8
-}
-
-// Decode Splice Info Section values.
-func (infosec *InfoSection) Decode(bd *bitter.Decoder) bool {
-	infosec.Name = "Splice Info Section"
-	infosec.TableID = bd.Hex(8)
-	if infosec.TableID != "0xfc" {
-		return false
-	}
-	infosec.SectionSyntaxIndicator = bd.Flag()
-	infosec.Private = bd.Flag()
-	infosec.Reserved = bd.Hex(2)
-	infosec.SectionLength = bd.UInt16(12)
-	infosec.ProtocolVersion = bd.UInt8(8)
-	infosec.EncryptedPacket = bd.Flag()
-	infosec.EncryptionAlgorithm = bd.UInt8(6)
-	infosec.PtsAdjustment = bd.As90k(33)
-	infosec.CwIndex = bd.Hex(8)
-	infosec.Tier = bd.Hex(12)
-	infosec.CommandLength = bd.UInt16(12)
-	infosec.CommandType = bd.UInt8(8)
-
-	return true
-}
-
-// Defaults sets default InfoSection values for encoding
-func (infosec *InfoSection) Defaults() {
-	infosec.Name = "Splice Info Section"
-	infosec.TableID = "0xfc"
-	infosec.SectionSyntaxIndicator = false
-	infosec.Private = false
-	infosec.Reserved = "0x3"
-	//infosec.SectionLength = 17
-	infosec.ProtocolVersion = 0
-	infosec.EncryptedPacket = false
-	infosec.EncryptionAlgorithm = 0
-	infosec.PtsAdjustment = 0.0
-	infosec.CwIndex = "0x0"
-	infosec.Tier = "0xfff"
-	infosec.CommandLength = 0
-
-	infosec.CommandType = 0
-}
-
 /*
 *
+Upid is the Struct for Segmentation Upids
 
-Encode Splice Info Section
-Encodes the InfoSection variables to bytes.
+		    These UPID types are recognized.
+
+	            0x01: "Deprecated",
+	            0x02: "Deprecated",
+	            0x03: "AdID",
+	            0x05: "ISAN"
+	            0x06: "ISAN"
+	            0x07: "TID",
+	            0x08: "AiringID",
+	            0x09: "ADI",
+	            0x10: "UUID",
+	            0x11: "ACR",
+	            0x0a: "EIDR",
+	            0x0b: "ATSC",
+	            0x0c: "MPU",
+	            0x0d: "MID",
+	            0x0e: "ADS Info",
+	            0x0f: "URI",
+
+		    Non-standard UPID types are returned as bytes.
+
 *
 */
-func (infosec *InfoSection) Encode() []byte {
-	//	infosec.Defaults()
-	be := &bitter.Encoder{}
-	be.Add16(uint16(0xfc), 16)
-	be.Add8(48, 8)
-	be.Add8(uint8(infosec.SectionLength), 8)
-	be.Add8(infosec.ProtocolVersion, 8)
-	be.AddFlag(infosec.EncryptedPacket)
-	be.Add8(infosec.EncryptionAlgorithm, 6)
-	be.Add90k(infosec.PtsAdjustment, 33)
-	be.AddHex64(infosec.CwIndex, 8)
-	be.AddHex64(infosec.Tier, 12)
-	be.Add16(infosec.CommandLength, 12)
-	be.Add8(infosec.CommandType, 8)
-	return be.Bites.Bytes()
+type Upid struct {
+	Name             string `json:",omitempty"`
+	UpidType         uint8  `json:",omitempty"`
+	Value            string `json:",omitempty"`
+	TSID             uint16 `json:",omitempty"`
+	Reserved         uint8  `json:",omitempty"`
+	EndOfDay         uint8  `json:",omitempty"`
+	UniqueFor        uint16 `json:",omitempty"`
+	ContentID        []byte `json:",omitempty"`
+	Upids            []Upid `json:",omitempty"`
+	FormatIdentifier string `json:",omitempty"`
+	PrivateData      []byte `json:",omitempty"`
+}
 
+// Decode Decodes Segmentation UPIDs
+func (upid *Upid) Decode(bd *bitter.Decoder, upidType uint8, upidlen uint8) {
+
+	upid.UpidType = upidType
+
+	var uri_upids = map[uint8]string{
+		0x01: "Deprecated",
+		0x02: "Deprecated",
+		0x03: "AdID",
+		0x07: "TID",
+		0x08: "AiringID",
+		0x09: "ADI",
+		0x10: "UUID",
+		0x11: "ACR",
+		0x0a: "EIDR",
+		0x0b: "ATSC",
+		0x0c: "MPU",
+		0x0d: "MID",
+		0x0e: "ADS Info",
+		0x0f: "URI",
+	}
+
+	name, ok := uri_upids[upidType]
+	if ok {
+		upid.Name = name
+		upid.uri(bd, upidlen)
+	} else {
+
+		switch upidType {
+
+		case 0x05, 0x06:
+			upid.Name = "ISAN"
+			upid.isan(bd, upidlen)
+		case 0x08:
+			upid.Name = "AiringID"
+			upid.airid(bd, upidlen)
+		case 0x0a:
+			upid.Name = "EIDR"
+			upid.eidr(bd, upidlen)
+		case 0x0b:
+			upid.Name = "ATSC"
+			upid.atsc(bd, upidlen)
+		case 0x0c:
+			upid.Name = "MPU"
+			upid.mpu(bd, upidlen)
+		case 0x0d:
+			upid.Name = "MID"
+			upid.mid(bd, upidlen)
+		default:
+			upid.Name = "UPID"
+			upid.uri(bd, upidlen)
+		}
+	}
+}
+
+// Decode for AirId
+func (upid *Upid) airid(bd *bitter.Decoder, upidlen uint8) {
+	upid.Value = bd.Hex(uint(upidlen << 3))
+}
+
+// Decode for Isan Upid
+func (upid *Upid) isan(bd *bitter.Decoder, upidlen uint8) {
+	upid.Value = bd.Ascii(uint(upidlen << 3))
+}
+
+// Decode for URI Upid
+func (upid *Upid) uri(bd *bitter.Decoder, upidlen uint8) {
+	upid.Value = bd.Ascii(uint(upidlen) << 3)
+}
+
+// Decode for ATSC Upid
+func (upid *Upid) atsc(bd *bitter.Decoder, upidlen uint8) {
+	upid.TSID = bd.UInt16(16)
+	upid.Reserved = bd.UInt8(2)
+	upid.EndOfDay = bd.UInt8(5)
+	upid.UniqueFor = bd.UInt16(9)
+	upid.ContentID = bd.Bytes(uint((upidlen - 4) << 3))
+}
+
+// Decode for EIDR Upid
+func (upid *Upid) eidr(bd *bitter.Decoder, upidlen uint8) {
+	if upidlen == 12 {
+		head := bd.UInt64(16)
+		tail := bd.Hex(80)
+		upid.Value = fmt.Sprintf("10%v/%v", head, tail)
+	}
+}
+
+// Decode for MPU Upid
+func (upid *Upid) mpu(bd *bitter.Decoder, upidlen uint8) {
+	ulb := uint(upidlen) << 3
+	upid.FormatIdentifier = bd.Hex(32)
+	upid.PrivateData = bd.Bytes(ulb - 32)
+}
+
+// Decode for MID Upid
+func (upid *Upid) mid(bd *bitter.Decoder, upidlen uint8) {
+	var i uint8
+	i = 0
+	for i < upidlen {
+		utype := bd.UInt8(8)
+		i++
+		ulen := bd.UInt8(8)
+		i++
+		i += ulen
+		var mupid Upid
+		upid.Decode(bd, utype, ulen)
+		upid.Upids = append(upid.Upids, mupid)
+	}
 }
