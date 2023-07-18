0ee8d3dbd28fda2d48bc57397eab0304fff4f19d
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-18 18:13:31 +0000

    unexporting CRC32 constants

--- crc32.go
+++ crc32.go
@@ -1,47 +1,48 @@
 package cuei
 
 // Polynomial value for CRC32 table
-const POLY = 0x104C11DB7
-// initial Crc32 value
-const INIT_VALUE = 0xFFFFFFFF
-// Crc32 mask
-const MASK  = 0x80000000
-const ZERO = 0x0
-const ONE = 0x1
-const EIGHT = 0x8
-const TWENTY_FOUR = 0x18
-const TWO_FIFTY_FIVE = 0xFF
-const TWO_FIFTY_SIX = 0x100
+const initPoly = 0x104C11DB7
 
+// initial Crc32 value
+const initValue= 0xFFFFFFFF
+
+// Crc32 mask
+const mask = 0x80000000
+const zero = 0x0
+const one = 0x1
+const eight = 0x8
+const twentyFour = 0x18
+const twoFiftyFive= 0xFF
+const twoFiftySix = 0x100
 
 // bytecrc creates the values used to populate the table
-func bytecrc(crc int, poly int) int {
-	for i := 0; i < EIGHT; i++ {
-		if crc& MASK != ZERO {
-			crc = crc<<ONE ^ poly
+func bytecrc(crc int, aPoly int) int {
+	for i := 0; i < eight; i++ {
+		if crc& mask != zero {
+			crc = crc<<one ^ aPoly
 		} else {
-			crc = crc << ONE
+			crc = crc << one
 		}
 	}
-	return int(crc & INIT_VALUE)
+	return int(crc &initValue)
 }
 
 // mkTable makes the Crc32 table
 func mkTable() [256]int {
-	var tbl [TWO_FIFTY_SIX]int
-	poly := POLY & INIT_VALUE
+	var tbl [twoFiftySix]int
+	newPoly := initPoly & initValue
 	for idx, _ := range tbl {
-		tbl[idx] = (bytecrc((idx << TWENTY_FOUR), poly))
+		tbl[idx] = (bytecrc((idx << twentyFour), newPoly))
 	}
 	return tbl
 }
 
 // generate a 32 bit Crc
 func CRC32(data []byte) uint32 {
-	crc := INIT_VALUE
+	crc := initValue
 	tbl := mkTable()
 	for _, bite := range data {
-		crc = tbl[int(bite)^((crc>>TWENTY_FOUR)&TWO_FIFTY_FIVE)] ^ ((crc << EIGHT) & (INIT_VALUE - TWO_FIFTY_FIVE))
+		crc = tbl[int(bite)^((crc>>twentyFour)& twoFiftyFive)] ^ ((crc << eight) & (initValue- twoFiftyFive))
 	}
 	return uint32(crc)
 }
