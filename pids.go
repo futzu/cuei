a6d21f7bacedfe8c74545b4111b452261bd75476
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-18 18:42:27 +0000

    unexporting

--- pids.go
+++ pids.go
@@ -8,7 +8,7 @@ type Pids struct {
 }
 
 func (pids *Pids) isPmtPid(pid uint16) bool {
-	return IsIn(pids.PmtPids, pid)
+	return isIn(pids.PmtPids, pid)
 }
 
 func (pids *Pids) addPmtPid(pid uint16) {
@@ -18,7 +18,7 @@ func (pids *Pids) addPmtPid(pid uint16) {
 }
 
 func (pids *Pids) isPcrPid(pid uint16) bool {
-	return IsIn(pids.PcrPids, pid)
+	return isIn(pids.PcrPids, pid)
 }
 
 func (pids *Pids) addPcrPid(pid uint16) {
@@ -28,7 +28,7 @@ func (pids *Pids) addPcrPid(pid uint16) {
 }
 
 func (pids *Pids) isScte35Pid(pid uint16) bool {
-	return IsIn(pids.Scte35Pids, pid)
+	return isIn(pids.Scte35Pids, pid)
 }
 
 func (pids *Pids) addScte35Pid(pid uint16) {
