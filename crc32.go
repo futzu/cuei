86fcc892f5438f0ff3928f7009e93bbbe79b6f2a
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-24 19:45:04 +0000

    Simplified

--- crc32.go
+++ crc32.go
@@ -2,29 +2,33 @@ package cuei
 
 // Polynomial value for cRC32 table
 const initPoly = 0x104C11DB7
-
 // initial Crc32 value
-const initValue= 0xFFFFFFFF
-
+const initValue = 0xFFFFFFFF
 // Crc32 mask
 const mask = 0x80000000
+// zero 
 const zero = 0x0
+// one
 const one = 0x1
+// eight
 const eight = 0x8
+// twenty-four
 const twentyFour = 0x18
-const twoFiftyFive= 0xFF
+// two hundred and fifty-five
+const twoFiftyFive = 0xFF
+// two hundred and fifty-six
 const twoFiftySix = 0x100
 
 // bytecrc creates the values used to populate the table
 func bytecrc(crc int, aPoly int) int {
 	for i := 0; i < eight; i++ {
-		if crc& mask != zero {
+		if crc&mask != zero {
 			crc = crc<<one ^ aPoly
 		} else {
 			crc = crc << one
 		}
 	}
-	return int(crc &initValue)
+	return int(crc & initValue)
 }
 
 // mkTable makes the Crc32 table
@@ -42,7 +46,7 @@ func cRC32(data []byte) uint32 {
 	crc := initValue
 	tbl := mkTable()
 	for _, bite := range data {
-		crc = tbl[int(bite)^((crc>>twentyFour)& twoFiftyFive)] ^ ((crc << eight) & (initValue- twoFiftyFive))
+		crc = tbl[int(bite)^((crc>>twentyFour)&twoFiftyFive)] ^ ((crc << eight) & (initValue - twoFiftyFive))
 	}
 	return uint32(crc)
 }
