930abf51cb802403081c11d5c0bb53c7c7dfcd8d
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-29 23:56:08 +0000

    Added more examples

--- example_test.go
+++ example_test.go
@@ -1,9 +1,22 @@
 package cuei_test
 
 import (
+	"fmt"
 	"github.com/futzu/cuei"
 	"testing"
 )
+
+func ExampleDecB64() {
+	b64 := "/DARAAAAAAAAAP/wAAAAAHpPv/8="
+	dcoded := cuei.DecB64(b64)
+	fmt.Println(dcoded)
+}
+
+func ExampleEncB64() {
+	somebytes := []byte{252, 48, 17, 0, 0, 0, 0, 0, 0, 0, 255, 240, 0, 0, 0, 0, 122, 79, 191, 255}
+	ncoded := cuei.EncB64(somebytes)
+	fmt.Println(ncoded)
+}
 
 func ExampleJson2Cue() {
 
@@ -49,8 +62,28 @@ func ExampleJson2Cue() {
 	cue.Show()
 }
 
+func ExampleNewCue() {
+
+	data := "/DCtAAAAAAAAAP/wBQb+Tq9DwQCXAixDVUVJCUvhcH+fAR1QQ1IxXzEyMTYyMTE0MDBXQUJDUkFDSEFFTFJBWSEBAQIsQ1VFSQlL4W9/nwEdUENSMV8xMjE2MjExNDAwV0FCQ1JBQ0hBRUxSQVkRAQECGUNVRUkJTBwVf58BClRLUlIxNjA4NEEQAQECHkNVRUkJTBwWf98AA3clYAEKVEtSUjE2MDg0QSABAdHBXYA="
+
+	cue := cuei.NewCue()
+	cue.Decode(cuei.DecB64(data))
+	cue.Show()
+}
+
 func Test(t *testing.T) {
-	t.Run("A Test", func(t *testing.T) {
+
+	t.Run("DecB64", func(t *testing.T) {
+		ExampleDecB64()
+	})
+	t.Run("EncB64", func(t *testing.T) {
+		ExampleEncB64()
+	})
+	t.Run("Json2Cue", func(t *testing.T) {
 		ExampleJson2Cue()
 	})
+	t.Run("NewCue", func(t *testing.T) {
+		ExampleNewCue()
+	})
+
 }
