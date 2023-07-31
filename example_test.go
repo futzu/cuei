c6b2c5d1f55d9a83f5f26f30ecbacd1d39c40fcf
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-31 00:07:17 +0000

    Six new doc examples

--- example_test.go
+++ example_test.go
@@ -63,12 +63,53 @@ func ExampleJson2Cue() {
 }
 
 func ExampleNewCue() {
-
 	data := "/DCtAAAAAAAAAP/wBQb+Tq9DwQCXAixDVUVJCUvhcH+fAR1QQ1IxXzEyMTYyMTE0MDBXQUJDUkFDSEFFTFJBWSEBAQIsQ1VFSQlL4W9/nwEdUENSMV8xMjE2MjExNDAwV0FCQ1JBQ0hBRUxSQVkRAQECGUNVRUkJTBwVf58BClRLUlIxNjA4NEEQAQECHkNVRUkJTBwWf98AA3clYAEKVEtSUjE2MDg0QSABAdHBXYA="
 
 	cue := cuei.NewCue()
-	cue.Decode(cuei.DecB64(data))
+	cue.Decode(data)
 	cue.Show()
+}
+
+func ExampleCue_Decode() {
+	data := "/DAWAAAAAAAAAP/wBQb+AKmKxwAACzuu2Q=="
+	cue := cuei.NewCue()
+	cue.Decode(data)
+	cue.Show()
+}
+
+func ExampleCue_Encode() {
+	data := "/DAWAAAAAAAAAP/wBQb+AKmKxwAACzuu2Q=="
+	cue := cuei.NewCue()
+	cue.Decode(data)
+	fmt.Println(cue.Encode())
+}
+
+func ExampleCue_Encode2B64() {
+	data := "/DAWAAAAAAAAAP/wBQb+AKmKxwAACzuu2Q=="
+	cue := cuei.NewCue()
+	cue.Decode(data)
+	fmt.Println(cue.Encode2B64())
+}
+
+func ExampleCue_Encode2Hex() {
+	data := "/DAWAAAAAAAAAP/wBQb+AKmKxwAACzuu2Q=="
+	cue := cuei.NewCue()
+	cue.Decode(data)
+	fmt.Println(cue.Encode2Hex())
+	cue.Decode(cue.Encode2Hex())
+	cue.Show()
+}
+
+func ExampleCue_AdjustPts() {
+	data := "/DAWAAAAAAAAAP/wBQb+AKmKxwAACzuu2Q=="
+	cue := cuei.NewCue()
+	cue.Decode(data)
+	cue.Show()
+	// Change cue.InfoSection.PtsAdjustment and re-encode cue to bytes
+	cue.AdjustPts(33.333)
+	cue.Show()
+	fmt.Println(data)
+	fmt.Println(cuei.EncB64(cue.Encode()))
 }
 
 func Test(t *testing.T) {
@@ -82,8 +123,22 @@ func Test(t *testing.T) {
 	t.Run("Json2Cue", func(t *testing.T) {
 		ExampleJson2Cue()
 	})
+	t.Run("Cue.Decode()", func(t *testing.T) {
+		ExampleCue_Decode()
+	})
+	t.Run("Cue.AdjustPts()", func(t *testing.T) {
+		ExampleCue_AdjustPts()
+	})
 	t.Run("NewCue", func(t *testing.T) {
 		ExampleNewCue()
 	})
-
+	t.Run("Cue_Encode", func(t *testing.T) {
+		ExampleCue_Encode()
+	})
+	t.Run("Cue_Encode2B64", func(t *testing.T) {
+		ExampleCue_Encode2B64()
+	})
+	t.Run("Cue_Encode2Hex", func(t *testing.T) {
+		ExampleCue_Encode2Hex()
+	})
 }
