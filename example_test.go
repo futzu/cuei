59c26bfc775314edd7d938ab867d80f0216b9e1b
Adrian <52701496+futzu@users.noreply.github.com>
2023-08-06 21:25:28 +0000

    Removed EncB64 and DecB64 examples

--- example_test.go
+++ example_test.go
@@ -6,18 +6,6 @@ import (
 	"testing"
 )
 
-func ExampleDecB64() {
-	b64 := "/DARAAAAAAAAAP/wAAAAAHpPv/8="
-	dcoded := cuei.DecB64(b64)
-	fmt.Println(dcoded)
-}
-
-func ExampleEncB64() {
-	somebytes := []byte{252, 48, 17, 0, 0, 0, 0, 0, 0, 0, 255, 240, 0, 0, 0, 0, 122, 79, 191, 255}
-	ncoded := cuei.EncB64(somebytes)
-	fmt.Println(ncoded)
-}
-
 func ExampleJson2Cue() {
 
 	js := `{
@@ -114,12 +102,6 @@ func ExampleCue_AdjustPts() {
 
 func Test(t *testing.T) {
 
-	t.Run("DecB64", func(t *testing.T) {
-		ExampleDecB64()
-	})
-	t.Run("EncB64", func(t *testing.T) {
-		ExampleEncB64()
-	})
 	t.Run("Json2Cue", func(t *testing.T) {
 		ExampleJson2Cue()
 	})
