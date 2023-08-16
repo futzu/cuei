4c39b73fd65e36d85cf71fba4efdfbcc99849645
Adrian <52701496+futzu@users.noreply.github.com>
2023-08-16 06:44:43 +0000

    MkCrc32

--- crc32.go
+++ crc32.go
@@ -1,23 +1,14 @@
 package cuei
 
-// Polynomial value for cRC32 table
-const initPoly = 0x104C11DB7
-// initial Crc32 value
-const initValue = 0xFFFFFFFF
-// Crc32 mask
-const mask = 0x80000000
-// zero 
-const zero = 0x0
-// one
-const one = 0x1
-// eight
-const eight = 0x8
-// twenty-four
+const zero = 0x00
+const one = 0x01
+const eight = 0x08
 const twentyFour = 0x18
-// two hundred and fifty-five
 const twoFiftyFive = 0xFF
-// two hundred and fifty-six
 const twoFiftySix = 0x100
+const mask = 0x80000000      // Crc32 mask
+const initValue = 0xFFFFFFFF // initial Crc32 value
+const initPoly = 0x104C11DB7 // Polynomial value for cRC32 table
 
 // bytecrc creates the values used to populate the table
 func bytecrc(crc int, aPoly int) int {
@@ -32,17 +23,17 @@ func bytecrc(crc int, aPoly int) int {
 }
 
 // mkTable makes the Crc32 table
-func mkTable() [256]int {
+func mkTable() [twoFiftySix]int {
 	var tbl [twoFiftySix]int
 	newPoly := initPoly & initValue
 	for idx, _ := range tbl {
-		tbl[idx] = (bytecrc((idx << twentyFour), newPoly))
+		tbl[idx] = bytecrc((idx << twentyFour), newPoly)
 	}
 	return tbl
 }
 
-// generate a 32 bit Crc
-func cRC32(data []byte) uint32 {
+// MkCrc32 generate a 32 bit Crc
+func MkCrc32(data []byte) uint32 {
 	crc := initValue
 	tbl := mkTable()
 	for _, bite := range data {
