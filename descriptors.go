11bf254f6d7bd5bb08f4079c76dcd86f1d38e8b5
Adrian of Doom <52701496+futzu@users.noreply.github.com>
2024-11-16 11:17:50 -0500

    all in on Splice Descriptor

--- descriptors.go
+++ descriptors.go
@@ -5,32 +5,31 @@ import (
 	"fmt"
 )
 
+// Tag, Length , Name and Identifier for Descriptors
+type TagLenNameId struct {
+	Tag        uint8
+	Length     uint8
+	Name       string
+	Identifier string
+}
+
 // Avail Descriptor
-type availDescriptor struct {
-	Tag             uint8
-	Length          uint8
-	Identifier      string
-	Name            string
+type AvailDescriptor struct {
+	TagLenNameId
 	ProviderAvailID uint32
 }
 
 // DTMF Descriptor
-type dtmfDescriptor struct {
-	Tag        uint8
-	Length     uint8
-	Identifier string
-	Name       string
-	PreRoll    uint8
-	DTMFCount  uint8
-	DTMFChars  uint64
+type DTMFDescriptor struct {
+	TagLenNameId
+	PreRoll   uint8
+	DTMFCount uint8
+	DTMFChars uint64
 }
 
 // Segmentation Descriptor
-type segmentationDescriptor struct {
-	Tag                                    uint8
-	Length                                 uint8
-	Identifier                             string
-	Name                                   string
+type SegmentationDescriptor struct {
+	TagLenNameId
 	SegmentationEventID                    string
 	SegmentationEventCancelIndicator       bool
 	SegmentationEventIDComplianceIndicator bool
@@ -49,16 +48,13 @@ type segmentationDescriptor struct {
 	SegmentationTypeID                     uint8
 	SegmentNum                             uint8
 	SegmentsExpected                       uint8
-	SubSegmentNum                          uint8 `json:",omitempty"`
-	SubSegmentsExpected                    uint8 `json:",omitempty"`
+	SubSegmentNum                          uint8
+	SubSegmentsExpected                    uint8
 }
 
 // Time Descriptor
-type timeDescriptor struct {
-	Tag        uint8
-	Length     uint8
-	Identifier string
-	Name       string
+type TimeDescriptor struct {
+	TagLenNameId
 	TAISeconds uint64
 	TAINano    uint32
 	UTCOffset  uint16
@@ -67,140 +63,45 @@ type timeDescriptor struct {
 /*
 *
 
-		The Descriptor Struct
-	    is used for all supported descriptors.
-
-	 	    0x0: AvailDescriptor,
-		    0x1: DTMFDescriptor,
-		    0x2: SegmentationDescriptor
-	        0x3: TimeDescriptor
-
-	    It may sound a bit weird but it works really well and it's easy.
+	Descriptor is the combination of all the descriptors
+	this is to maintain dot notation in the Cue struct.
 
 *
 */
-type Descriptor struct { // Used by
-	Tag                                    uint8   // All
-	Length                                 uint8   //  .
-	Identifier                             string  //  .
-	Name                                   string  //  .
-	ProviderAvailID                        uint32  // Avail
-	PreRoll                                uint8   // DTMF
-	DTMFCount                              uint8   //  .
-	DTMFChars                              uint64  //  .
-	SegmentationEventID                    string  // Segmentation
-	SegmentationEventCancelIndicator       bool    //  .
-	SegmentationEventIDComplianceIndicator bool    //  .
-	ProgramSegmentationFlag                bool    //  .
-	SegmentationDurationFlag               bool    //  .
-	DeliveryNotRestrictedFlag              bool    //  .
-	WebDeliveryAllowedFlag                 bool    //  .
-	NoRegionalBlackoutFlag                 bool    //  .
-	ArchiveAllowedFlag                     bool    //  .
-	DeviceRestrictions                     string  //  .
-	SegmentationDuration                   float64 //  .
-	SegmentationMessage                    string  //  .
-	SegmentationUpidType                   uint8   //  .
-	SegmentationUpidLength                 uint8   //  .
-	SegmentationUpid                       *Upid   //  .
-	SegmentationTypeID                     uint8   //  .
-	SegmentNum                             uint8   //  .
-	SegmentsExpected                       uint8   //  .
-	SubSegmentNum                          uint8   //  .
-	SubSegmentsExpected                    uint8   //  .
-	TAISeconds                             uint64  // Time
-	TAINano                                uint32  //  .
-	UTCOffset                              uint16  //  .
-}
-
-func (dscptr *Descriptor) jsonAvailDescriptor() ([]byte, error) {
-	avail := &availDescriptor{
-		Tag:             dscptr.Tag,
-		Length:          dscptr.Length,
-		Identifier:      dscptr.Identifier,
-		Name:            dscptr.Name,
-		ProviderAvailID: dscptr.ProviderAvailID}
-	return json.Marshal(avail)
-}
-
-func (dscptr *Descriptor) jsonDTMFDescriptor() ([]byte, error) {
-	dtmf := &dtmfDescriptor{
-		Tag:        dscptr.Tag,
-		Length:     dscptr.Length,
-		Identifier: dscptr.Identifier,
-		Name:       dscptr.Name,
-		PreRoll:    dscptr.PreRoll,
-		DTMFCount:  dscptr.DTMFCount,
-		DTMFChars:  dscptr.DTMFChars}
-	return json.Marshal(&dtmf)
-}
-
-func (dscptr *Descriptor) jsonSegmentationDescriptor() ([]byte, error) {
-	seg := &segmentationDescriptor{
-		Tag:                                    dscptr.Tag,
-		Length:                                 dscptr.Length,
-		Identifier:                             dscptr.Identifier,
-		Name:                                   dscptr.Name,
-		SegmentationEventID:                    dscptr.SegmentationEventID,
-		SegmentationEventCancelIndicator:       dscptr.SegmentationEventCancelIndicator,
-		SegmentationEventIDComplianceIndicator: dscptr.SegmentationEventIDComplianceIndicator,
-		ProgramSegmentationFlag:                dscptr.ProgramSegmentationFlag,
-		SegmentationDurationFlag:               dscptr.SegmentationDurationFlag,
-		DeliveryNotRestrictedFlag:              dscptr.DeliveryNotRestrictedFlag,
-		WebDeliveryAllowedFlag:                 dscptr.WebDeliveryAllowedFlag,
-		NoRegionalBlackoutFlag:                 dscptr.NoRegionalBlackoutFlag,
-		ArchiveAllowedFlag:                     dscptr.ArchiveAllowedFlag,
-		DeviceRestrictions:                     dscptr.DeviceRestrictions,
-		SegmentationDuration:                   dscptr.SegmentationDuration,
-		SegmentationMessage:                    dscptr.SegmentationMessage,
-		SegmentationUpidType:                   dscptr.SegmentationUpidType,
-		SegmentationUpidLength:                 dscptr.SegmentationUpidLength,
-		SegmentationUpid:                       dscptr.SegmentationUpid,
-		SegmentationTypeID:                     dscptr.SegmentationTypeID,
-		SegmentNum:                             dscptr.SegmentNum,
-		SegmentsExpected:                       dscptr.SegmentsExpected,
-		SubSegmentNum:                          dscptr.SubSegmentNum,
-		SubSegmentsExpected:                    dscptr.SubSegmentsExpected}
-
-	return json.Marshal(seg)
-}
-
-func (dscptr *Descriptor) jsonTimeDescriptor() ([]byte, error) {
-	timed := &timeDescriptor{
-		Tag:        dscptr.Tag,
-		Length:     dscptr.Length,
-		Identifier: dscptr.Identifier,
-		Name:       dscptr.Name,
-		TAISeconds: dscptr.TAISeconds,
-		TAINano:    dscptr.TAINano,
-		UTCOffset:  dscptr.UTCOffset}
-
-	return json.Marshal(timed)
+type Descriptor struct {
+	TagLenNameId
+	AvailDescriptor
+	DTMFDescriptor
+	SegmentationDescriptor
+	TimeDescriptor
 }
 
 /*
-*
+		 *
+		    Custom MarshalJSON
+		        Marshal a Descriptor into
 
-			Custom MarshalJSON
-
-	   		Marshal a Descriptor into
-			    0x0: AvailDescriptor,
+	            0x0: AvailDescriptor,
 			    0x1: DTMFDescriptor,
-			    0x2: SegmentationDescriptor
-			    0x3: TimeDescriptor
+			    0x2: SegmentationDescriptor,
+			    0x3: TimeDescriptor,
+		        or just return the Descriptor
 
 *
 */
 func (dscptr *Descriptor) MarshalJSON() ([]byte, error) {
 	switch dscptr.Tag {
 	case 0x0:
-		return dscptr.jsonAvailDescriptor()
+		return json.Marshal(&dscptr.AvailDescriptor)
+
 	case 0x1:
-		return dscptr.jsonDTMFDescriptor()
+		return json.Marshal(&dscptr.DTMFDescriptor)
+
 	case 0x2:
-		return dscptr.jsonSegmentationDescriptor()
+		return json.Marshal(&dscptr.SegmentationDescriptor)
+
 	case 0x3:
-		return dscptr.jsonTimeDescriptor()
+		return json.Marshal(&dscptr.TimeDescriptor)
 	}
 	type Funk Descriptor
 	return json.Marshal(&struct{ *Funk }{(*Funk)(dscptr)})
@@ -220,14 +121,14 @@ func (dscptr *Descriptor) Show() {
 
 /*
 *
-Decode a Splice Descriptor by tag.
+Decode returns a Splice Descriptor by tag.
 
 	The following Splice Descriptors are recognized.
 
 	    0x0: Avail Descriptor,
 	    0x1: DTMF Descriptor,
 	    0x2: Segmentation Descriptor,
-	    0x3: Time Descriptor
+	    0x3: Time Descriptor,
 
 *
 */
@@ -248,6 +149,7 @@ func (dscptr *Descriptor) decode(bd *bitDecoder, tag uint8, length uint8) {
 	}
 }
 
+
 // Decode for  Avail Descriptors
 func (dscptr *Descriptor) decodeAvailDescriptor(bd *bitDecoder, tag uint8, length uint8) {
 	dscptr.Tag = tag
@@ -255,6 +157,7 @@ func (dscptr *Descriptor) decodeAvailDescriptor(bd *bitDecoder, tag uint8, lengt
 	dscptr.Identifier = bd.asAscii(32)
 	dscptr.Name = "Avail Descriptor"
 	dscptr.ProviderAvailID = bd.uInt32(32)
+	dscptr.AvailDescriptor.TagLenNameId = dscptr.TagLenNameId
 
 }
 
@@ -268,6 +171,7 @@ func (dscptr *Descriptor) decodeDTMFDescriptor(bd *bitDecoder, tag uint8, length
 	dscptr.DTMFCount = bd.uInt8(3)
 	//bd.goForward(5)
 	dscptr.DTMFChars = bd.uInt64(uint(8 * dscptr.DTMFCount))
+	dscptr.DTMFDescriptor.TagLenNameId = dscptr.TagLenNameId
 
 }
 
@@ -280,6 +184,7 @@ func (dscptr *Descriptor) decodeTimeDescriptor(bd *bitDecoder, tag uint8, length
 	dscptr.TAISeconds = bd.uInt64(48)
 	dscptr.TAINano = bd.uInt32(32)
 	dscptr.UTCOffset = bd.uInt16(16)
+	dscptr.TimeDescriptor.TagLenNameId = dscptr.TagLenNameId
 
 }
 
@@ -297,6 +202,7 @@ func (dscptr *Descriptor) decodeSegmentationDescriptor(bd *bitDecoder, tag uint8
 		dscptr.decodeSegFlags(bd)
 		dscptr.decodeSegmentation(bd)
 	}
+	dscptr.SegmentationDescriptor.TagLenNameId = dscptr.TagLenNameId
 
 }
 
@@ -354,6 +260,8 @@ func (dscptr *Descriptor) encodeAvailDescriptor(be *bitEncoder) {
 
 // Encode a segmentation descriptor
 func (dscptr *Descriptor) encodeSegmentationDescriptor(be *bitEncoder) {
+	dscptr.SegmentationDescriptor.TagLenNameId = dscptr.TagLenNameId
+
 	be.AddHex64(dscptr.SegmentationEventID, 32)
 	be.Add(dscptr.SegmentationEventCancelIndicator, 1)
 	be.Add(dscptr.SegmentationEventIDComplianceIndicator, 1)
